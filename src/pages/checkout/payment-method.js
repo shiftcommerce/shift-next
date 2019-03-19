6528c32f08428ad8d3eee8a0c69a16e478486dee
Mufudzi Masaire <mufudzi.masaire@shiftcommerce.com>
2019-03-19 11:58:32 +0000

    Add PP callback funcs -- wip

--- src/pages/checkout/payment-method.js
+++ src/pages/checkout/payment-method.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react'
 import Router from 'next/router'
 
-// // Components
+// Components
 import {
   PaymentMethods
 } from '@shiftcommerce/shift-react-components'
@@ -10,11 +10,12 @@ import {
 export class PaymentMethodPage extends Component {
   constructor (props) {
     super(props)
-
     this.state = {
       loading: false
     }
     this.nextSection = this.nextSection.bind(this)
+    this.paypalCreateOrder = this.paypalCreateOrder.bind(this)
+    this.paypalOnApprove = this.paypalOnApprove.bind(this)
   }
 
   nextSection () {
@@ -29,15 +30,30 @@ export class PaymentMethodPage extends Component {
     return null
   }
 
-  render () {
+  paypalCreateOrder(data, actions) {
     const { cart } = this.props
+    return actions.order.create({
+      purchase_units: [{
+        amount: {
+          value: cart.total
+        }
+      }]
+    })
+  }
 
+  paypalOnApprove(data, actions) {
+    // @TODO - extract shipping address data
+    return data
+  }
+
+  render () {
     return (
       <div>
-        { this.state.loading ? <p>Loading...</p> : <PaymentMethods 
-          cart={cart}
+        { this.state.loading ? <p>Loading...</p> : <PaymentMethods
           nextSection={this.nextSection}
-          className='u-margin-top-l-des u-margin-top-l-mobtab u-margin-top-l-tab u-margin-top-l-mob'
+          paypalClientID='sb'
+          paypalCreateOrder={this.paypalCreateOrder}
+          paypalOnApprove={this.paypalOnApprove}
         /> }
       </div>
     )
