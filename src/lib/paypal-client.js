53ae843ef4d440f86b848c05b9bc37d7ad38ddf2
Mufudzi Masaire <mufudzi.masaire@shiftcommerce.com>
2019-03-23 06:06:19 +0000

    Add initial PayPalClient library with updateOrderTotal func -- wip

---
+++ src/lib/paypal-client.js
@@ -0,0 +1,70 @@
+// Libraries
+import checkoutNodeJssdk from '@paypal/checkout-server-sdk'
+
+import Config from '../lib/config'
+
+class PayPalClient {
+  constructor (clientId = Config.get().paypalClientID, clientSecret = Config.get().paypalClientSecret) {
+    this.client = new checkoutNodeJssdk.core.PayPalHttpClient(
+      new checkoutNodeJssdk.core.SandboxEnvironment(clientId, clientSecret)
+    )
+  }
+
+  /**
+   * Updates the order total
+   * @param  {integer} payPalOrderID
+   * @param  {string} purchaseUnitsReferenceID
+   * @param  {integer} cartTotal
+   * @param  {integer} cartSubTotal
+   * @param  {integer} shippingTotal
+   */
+  async updateOrderTotal (payPalOrderID, purchaseUnitsReferenceID, cartTotal, cartSubTotal, shippingTotal) {
+    const request = new checkoutNodeJssdk.orders.OrdersPatchRequest(payPalOrderID)
+    request.requestBody(this.buildPatchOrderPayload(purchaseUnitsReferenceID, cartTotal, cartSubTotal, shippingTotal))
+    try {
+      const response = await this.client.execute(request)
+      return { status: response.status, data: response.data }
+    } catch (error) {
+      console.error('PayPal Client: Error while patching order', error)
+      return { status: error.response.status, data: error }
+    }
+  }
+
+  /**
+   * Builds the patch order payload
+   * @param {string} purchaseUnitsReferenceID
+   * @param {integer} cartTotal
+   * @param {integer} cartSubTotal
+   * @param {integer} shippingTotal
+   * @param {string} currency_code
+   */
+  buildPatchOrderPayload (purchaseUnitsReferenceID, cartTotal, cartSubTotal, shippingTotal, currency_code = 'GBP') {
+    return [
+      {
+        'op': 'replace',
+        'path': '/intent',
+        'value': 'AUTHORISE'
+      },
+      {
+        'op': 'replace',
+        'path': `/purchase_units/@reference_id==${purchaseUnitsReferenceID}/amount`,
+        'value': {
+          'currency_code': `${currency_code}`,
+          'value': `${cartTotal}`,
+          'breakdown': {
+            'item_total': {
+              'currency_code': `${currency_code}`,
+              'value':  `${cartSubTotal}`
+            },
+            'shipping_total': {
+              'currency_code': `${currency_code}`,
+              'value':  `${shippingTotal}`
+            }
+          }
+        }
+      }
+    ]
+  }
+}
+
+export default PayPalClient
