39f0c66fac5db4ffd9819ddabc6f776568f01f3e
Owen Smith <ozz1649@gmail.com>
2019-03-26 11:53:02 +0000

    refactored page tests, general cleanup

--- src/actions/api-actions.js
+++ src/actions/api-actions.js
@@ -111,7 +111,6 @@ function _determinePostDispatch (dispatch, request, response) {
 
   if ((response.status === 202 || response.status === 201 || response.status === 200) && validationPassed) {
     if (request.successActionType) {
-      console.log(request.successActionType)
       const parsedPayload = new JsonApiParser().parse(response.data)
       // TODO: remove when all enpoints have been moved to shift-next
       const payload = parsedPayload || response.data
--- test/pages/password_reset.test.js
+++ test/pages/password_reset.test.js
@@ -1,18 +1,42 @@
 // Libraries
 import React from 'react'
-import { mount } from 'enzyme'
+import Router from 'next/router'
 
 // Pages
 import PasswordResetPage from '../../src/pages/password_reset'
 
-// InitialState
-import { initialState } from '../../src/reducers/set-account'
+// Actions
+import * as accountActions from '../../src/actions/account-actions'
 
-test('renders the password reset form', () => {
-  const wrapper = mount(<PasswordResetPage account={initialState} />)
+test('handleSubmit dispatches the action to reset password', async () => {
+  // Mocks
+  const passwordResetSpy = jest.spyOn(accountActions, 'passwordReset').mockImplementation(() => 'password reset response')
 
-  expect(wrapper).toMatchSnapshot()
-  expect(wrapper).toIncludeText('Password Reset')
-  expect(wrapper).toContainMatchingElement('input')
-  expect(wrapper).toContainMatchingElement('button')
+  // Arrange
+  const props = {
+    account: {
+      errors: []
+    },
+    token: 'dmf9B79spvNDUvA303030303'
+  }
+
+  const dispatch = jest.fn().mockImplementation(() => Promise.resolve(true))
+
+  const values = {
+    password: 'dmf9B79spvNDUvA',
+    confirmPassword: 'dmf9B79spvNDUvA'
+  }
+
+  // Act
+  const wrapper = shallow(
+    <PasswordResetPage dispatch={dispatch} account={props.account} token={props.token} />
+  )
+  wrapper.instance().handleSubmit(values)
+
+  // Assert
+  expect(dispatch).toHaveBeenCalledWith('password reset response')
+  expect(passwordResetSpy).toHaveBeenCalledTimes(1)
+  expect(passwordResetSpy).toHaveBeenCalledWith('dmf9B79spvNDUvA303030303', 'dmf9B79spvNDUvA')
+
+  passwordResetSpy.mockClear()
 })
