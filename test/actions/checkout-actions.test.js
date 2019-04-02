ea3fe618d76ac1334c457f042345a9319c41bf36
Mufudzi Masaire <mufudzi.masaire@shiftcommerce.com>
2019-04-02 06:08:11 +0100

    Update checkout-actions.test.js

--- test/actions/checkout-actions.test.js
+++ test/actions/checkout-actions.test.js
@@ -1,5 +1,7 @@
 import * as checkoutActions from '../../src/actions/checkout-actions'
+import * as OrderActions from '../../src/actions/order-actions'
 import * as actionTypes from '../../src/actions/action-types'
+import * as apiActions from '../../src/actions/api-actions'
 
 test('return SET_CHECKOUT_INPUT_VALUE on calling inputChange()', () => {
   // Arrange
@@ -23,7 +25,7 @@ test('return SET_CHECKOUT_INPUT_VALUE on calling inputChange()', () => {
   // Act
   fn(dispatch, getState)
 
-  // Asset
+  // Assert
   expect(fn).toEqual(expect.any(Function))
   expect(dispatch).toHaveBeenCalledWith({ payload: expectedPayload, type: actionTypes.SET_CHECKOUT_INPUT_VALUE })
 })
@@ -82,4 +84,47 @@ test('return SET_CHECKOUT_SHIPPING_ADDRESS on calling setCheckoutShippingAddress
 
   // Assert
   expect(result).toEqual({payload: expectedPayload, type: actionTypes.SET_CHECKOUT_SHIPPING_ADDRESS})
+})
+
+test('updatePayPalOrderTotal triggers a correct patchPayPalOrder request', () => {
+  // Arrange
+  const postSpy = jest.spyOn(apiActions, 'postEndpoint')
+  const payPalOrderID = '848484847'
+  const purchaseUnitsReferenceID = 'default'
+  const cart = {
+    sub_total: 18,
+    total: 20,
+    total_discount: 0,
+    tax: 1,
+    shipping_total: 1,
+    shipping_total_discount: 0,
+    free_shipping: false
+  }
+
+  // Act
+  checkoutActions.updatePayPalOrderTotal(payPalOrderID, purchaseUnitsReferenceID, cart)
+
+  // Assert
+  const requestObject = postSpy.mock.calls[0][0]
+  expect(requestObject.endpoint).toEqual('/patchPayPalOrder')
+  expect(requestObject.requestActionType).toEqual(actionTypes.PATCH_PAYPAL_ORDER)
+  expect(requestObject.errorActionType).toEqual(actionTypes.SET_PAYMENT_ERROR)
+  postSpy.mockRestore()
+})
+
+test('authorizePayPalOrder triggers a correct authorizePayPalOrder request', () => {
+  // Arrange
+  const postSpy = jest.spyOn(apiActions, 'postEndpoint')
+  const payPalOrderID = '848484847'
+
+  // Act
+  checkoutActions.authorizePayPalOrder(payPalOrderID)
+
+  // Assert
+  const requestObject = postSpy.mock.calls[0][0]
+  expect(requestObject.endpoint).toEqual('/authorizePayPalOrder')
+  expect(requestObject.requestActionType).toEqual(actionTypes.AUTHORIZE_PAYPAL_ORDER)
+  expect(requestObject.successActionType).toEqual(actionTypes.SET_ORDER_PAYPAL_AUTHORIZATION_DETAILS)
+  expect(requestObject.errorActionType).toEqual(actionTypes.SET_PAYMENT_ERROR)
+  postSpy.mockRestore()
 })
