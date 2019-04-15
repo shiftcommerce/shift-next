6c026efa448b6e56b92001bc56fa41bf523a4ed2
James Kirkby <james@breaksagency.com>
2019-04-15 13:47:53 +0100

    [0.8.3] Feature/Custom components into shift-next config (#38)
    * allow custom template components to be passed to shift-next using the shift-next-config
    
    * version bump
    
    * small change to comments

--- src/lib/render-components.js
+++ src/lib/render-components.js
@@ -1,29 +1,40 @@
 // Libraries
 import React, { Fragment } from 'react'
 
+// Lib
+import Config from './config'
+
 // Components
 import { buildTemplateComponentsManifest } from '@shiftcommerce/shift-react-components'
 
-export default function renderComponents (componentsData) {
-  let components = []
-  const templateComponentsManifest = buildTemplateComponentsManifest()
+/**
+ * Get the relevant templates for the page components
+ * @param  {Array}    pageComponents
+ * @return {String} - HTML markup for the component
+ */
+export default function renderComponents (pageComponents) {
+  // Merge any custom template components with the ones from shift-react-components
+  const templateComponentsManifest = Object.assign({}, buildTemplateComponentsManifest(), Config.get().customTemplateComponents)
 
-  for (let index = 0; index < componentsData.length; index++) {
-    let component = componentsData[index]
-    let ComponentName = templateComponentsManifest[component.reference]
+  // Loop through all of the page components from the platform
+  const componentsToRender = pageComponents.map((pageComponent, pageComponentIndex) => {
+    // Get the template from the manifest
+    const ComponentName = templateComponentsManifest[pageComponent.reference]
+
+    // If the template exists in the manifest, it should be rendered
     if (ComponentName) {
-      components.push(
+      return (
         <ComponentName
-          key={index}
-          componentData={component}
+          key={pageComponentIndex}
+          componentData={pageComponent}
         />
       )
     }
-  }
+  })
 
   return (
     <Fragment>
-      { components }
+      { componentsToRender }
     </Fragment>
   )
 }
