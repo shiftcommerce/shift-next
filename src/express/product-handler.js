a66fad5134f375caa100f7ecd1a7a724e20b32d7
Arron Fletcher <arron.fletcher@shiftcommerce.com>
2019-05-29 14:25:22 +0100

    [WIP] updated product request to specify includes & fields

--- src/express/product-handler.js
+++ src/express/product-handler.js
@@ -1,8 +1,17 @@
 const { SHIFTClient } = require('@shiftcommerce/shift-node-api')
 const { setSurrogateHeaders } = require('../lib/set-cache-headers')
 
+const productApiEndpointQuery = {
+  fields: {
+    asset_files: 'id,image_height,image_width,s3_url',
+    variants: 'id,meta_attributes,title,description,reference,ewis_eligible,product_id,sku,stock_allocated_level,stock_level,tax_code,stock_available_level,has_assets,price_includes_taxes,available_to_order,price,current_price,picture_url'
+  },
+  include: 'asset_files,variants,template,meta.*'
+}
+
 module.exports = {
   getProductById: async (req, res) => {
+    req.query = { ...req.query, ...productApiEndpointQuery }
     const response = await SHIFTClient.getProductV1(req.params.id, req.query)
 
     setSurrogateHeaders(response.headers, res)
